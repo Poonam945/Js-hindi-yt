ffaea3b522d7628079e8ffea642313af8c94e5d1
Just_a_dev <pk8313982@gmail.com>
2026-08-18 09:39:48 +0000

    variables

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -2,6 +2,19 @@ const accountId=144553
 let accountEmail="poonam@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
+let accountState
 
-//accountId=2
+//accountId=2 // node allowed
 console.log(accountId)
+
+/*
+Prefer not to use var bcz of issue
+in block scope and functional scope.
+*/
+
+accountEmail="hy@gmail.com"
+accountPassword="132689"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
