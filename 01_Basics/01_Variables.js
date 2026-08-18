9d85ffe1f90eff3d26e77e0a9e78893600cddf65
Just_a_dev <pk8313982@gmail.com>
2026-08-18 09:39:28 +0000

    datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="poonam@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2
+console.log(accountId)
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use starict";//treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs,not browser.
+
+let name="Poonam"// string data type
+let age=18//
+let isLoggedIn=false;//boolean data type
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>(ntlb abhi value assign nhi hui hai)
+//symbol=>unique
+
+//object
+
+console.log(typeof "Poonam")//string
+console.log(typeof null)//object(interview me ask)
+console.log(typeof undefined);//udefined
