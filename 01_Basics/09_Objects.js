c5cc1f9b295e722c5cfc4fcd13cfc10cc9cc5913
Just_a_dev <pk8313982@gmail.com>
2026-08-27 08:01:38 +0000

    objects

--- 01_Basics/09_Objects.js
+++ 01_Basics/09_Objects.js
@@ -9,7 +9,7 @@ const JsUser={
     name:"Hitesh",
     age:18,
     // mySym:"mykey1",//symbol lo ..object ki key me add kro..symbol ko as a key use krna hai
-    [mySym]:"mykey1",
+    [mySym]:"mykey1",//key ki taah act aise hogi-Interview me
     location:"Jaipur",
     email:"hitesh@google.com",
     isLoggedIn:false,
@@ -22,10 +22,19 @@ console.log(typeof JsUser.mySym)//string..means symbol ki tarh mySum use nhi ho 
 console.log(JsUser[mySym])//brackeet use krna padega access krne ke lie...//mykey1
 
 JsUser.email="hitesh@chatgpt.com"
-Object.freeze(JsUser)//taki email change na ho
+//Object.freeze(JsUser)//taki email change na ho
 JsUser.email="hitesh@microsoft.com"
+console.log(JsUser);
 
-//funcn ko variable ki tarah hot ahai js me
+//funcn ko variable ki tarah hota hai js me
 JsUser.greeting=function(){
-    
+    console.log("Hello JS User");   
 }
+JsUser.greetingTwo=function(){
+    console.log(`Hello JS user,${this.name}`);
+}
+console.log(JsUser.greeting());//Hello JS User
+console.log(JsUser.greetingTwo());
+
+//value ko acces krte waqt mainly . use krte hai
+//but special cases jaha square bracket use kia jata hai ["email"] aise
